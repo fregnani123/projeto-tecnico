98ffe7edf03d5aa6e5d2d0c72ebe83946adbc647
fregnani123 <fabianofregnani@hotmail.com>
2024-11-27 09:43:08 -0300

    fix: corrigir erro de definição de tipo para imagens no TypeScript

---
+++ client/src/assets/vite-env.d.ts
@@ -0,0 +1,9 @@
+// src/assets.d.ts
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg";
+declare module "*.bmp";
+declare module "*.tiff";
+
